c3127002a019f30cbb567017c339f7b9764f01e4
Timothy Miamba <timal0361@gmail.com>
2023-06-21 15:58:35 +0200

    Add operate function to index.js

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multipy = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const add = (firstNumber, secondNumber) => firstNumber + secondNumber;
+const subtract = (firstNumber, secondNumber) => firstNumber - secondNumber;
+const multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
+const divide = (firstNumber, secondNumber) => firstNumber / secondNumber;
+
+function operate(operator, firstNumber, secondNumber) {
+    /* This function receives an operator and operands. The operator is used to
+       determine which calculation function will be executed. The result of the
+       selected calculation is then return at the end of the function.*/
+
+    let result;
+    switch(operator) {
+        case '+':
+            result = add(firstNumber, secondNumber);
+            break;
+        case '-':
+            result = subtract(firstNumber, secondNumber);
+            break;
+        case '*':
+            result = multiply(firstNumber, secondNumber);
+            break;
+        case '/':
+            result = divide(firstNumber, secondNumber);
+            break;
+    }
+    return result;
+}
+
+let operator;
+let firstNumber;
+let secondNumber;
