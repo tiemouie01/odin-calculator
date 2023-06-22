1d8d24826675e86971ac396aa6f551016f674ccf
Timothy Miamba <timal0361@gmail.com>
2023-06-22 15:38:54 +0200

    Manipulate equals button function to display solutions for complex expressions

--- index.js
+++ index.js
@@ -30,19 +30,24 @@ function operate(operator, firstNumber, secondNumber) {
     return result;
 }
 
-function calculateSolution(display, operator, firstNumber, secondNumber) {
+function calculateSolution(display, signs, operands) {
+    //This function receives all operators and operands entered by the user.
+    
+    let operatorIndex = display.textContent.lastIndexOf(signs[signs.length - 1]) + 2;
+    operands.push(Number(display.textContent.slice(operatorIndex)));
+    //The final number entered by th user is collected using the above code.
 
-    /*When the equal sign is clicked on the calculator. The second number is collected from the display
-      and stored in a variable. All necessary parts of the calculation are then passed to the operate
-      function to perform the calculation and display the result.*/
-
-    let operatorIndex = display.textContent.indexOf(operator);
-    secondNumber = Number(display.textContent.slice(operatorIndex + 1).trimStart());
-    display.textContent = operate(operator, firstNumber, secondNumber);
+    let solution = operands[0];
+    for (let i = 0; i  < signs.length; i++) {
+        solution = operate(signs[i], solution, operands[i+1]); 
+    }
+    display.textContent = solution;
+    // A for loop is used to iteratively calculate each pair of expressions and a final solution is
+    // displayed when the loop ends.
 }
 
-let operands = [];
-let signs = [];
+let operands = []; // stores all the operands entered in a single expression.
+let signs = []; // stores all the operators clicked in a single expression.
 
 const numbers = document.querySelectorAll('.number');
 const display = document.querySelector('.display');
@@ -53,6 +58,10 @@ numbers.forEach(number => number.addEventListener('click',() => {
 
 const operators = document.querySelectorAll('.operator');
 operators.forEach(operator => operator.addEventListener('click', () => {
+    /* When an operator is pressed by the user. The sign of the operator is added on to the signs array.
+       The operand on the right side of the expression is appended to the operands array. These values
+       are later used to calculate the solution of the overall expression when the '=' is pressed.*/
+
     if (signs.length === 0) {
         operands.push(Number(display.textContent));
     } else {
@@ -64,12 +73,11 @@ operators.forEach(operator => operator.addEventListener('click', () => {
 }));
 
 const equals = document.querySelector('.equals');
-equals.addEventListener('click', () => calculateSolution(display,operator,firstNumber,secondNumber));
+equals.addEventListener('click', () => calculateSolution(display, signs, operands));
 
 const clear = document.querySelector('.clear');
 clear.addEventListener('click', () => {
-    firstNumber = 0;
-    secondNumber = 0;
-    operator = '';
+    operands = [];
+    signs = [];
     display.textContent = '';
-}); // The clear button clears the display and nullifies all variables to prepare for the next calculation.
+});
